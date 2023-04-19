ca6c9a2ca693882b530c8c8b108d40edf3ee6805
jadallred <allred.jade@gmail.com>
2023-04-18 20:12:09 -0600

    react

--- src/play/swimmingNarwhal.jsx
+++ src/play/swimmingNarwhal.jsx
@@ -1,157 +1,146 @@
 import React from 'react';
-import styled from "styled-components";
-import { useEffect, useState } from "react";
+
+import { Button } from 'react-bootstrap';
+import { delay } from './delay';
+import { GameEvent, GameNotifier } from './gameNotifier';
 import './swimmingNarwhal.css';
 
-const NARWHAL_HEIGHT = 28;
-const NARWHAL_WIDTH = 33;
-const WALL_HEIGHT = 600;
-const WALL_WIDTH = 400;
-const GRAVITY = 8;
-const OBJ_WIDTH = 52;
-const OBJ_SPEED = 6;
-const OBJ_GAP = 200;
-function App() {
-  const [isStart, setIsStart] = useState(false);
-  const [narwhalpos, setBirspos] = useState(300);
-  const [objHeight, setObjHeight] = useState(0);
-  const [objPos, setObjPos] = useState(WALL_WIDTH);
-  const [score, setScore] = useState(0);
-  useEffect(() => {
-    let intVal;
-    if (isStart && narwhalpos < WALL_HEIGHT - NARWHAL_HEIGHT) {
-      intVal = setInterval(() => {
-        setBirspos((narwhalpos) => narwhalpos + GRAVITY);
-      }, 24);
-    }
-    return () => clearInterval(intVal);
-  });
+export function SwimmingNarwhal(props) {
+  const userName = props.userName;
 
-  useEffect(() => {
-    let objval;
-    if (isStart && objPos >= -OBJ_WIDTH) {
-      objval = setInterval(() => {
-        setObjPos((objPos) => objPos - OBJ_SPEED);
-      }, 24);
+  const [allowPlayer, setAllowPlayer] = React.useState(false);
+  const [sequence, setSequence] = React.useState([]);
+  const [playbackPos, setPlaybackPos] = React.useState(0);
 
-      return () => {
-        clearInterval(objval);
+  function Game() {
+    const [moveSpeed, setMoveSpeed] = useState(2);
+    const [gravity, setGravity] = useState(0.24);
+    const [gameState, setGameState] = useState('Start');
+    const [score, setScore] = useState(0);
+    const [pipes, setPipes] = useState([]);
+    const [narwhalDy, setNarwhalDy] = useState(0);
+  
+    useEffect(() => {
+      const handleKeyDown = (e) => {
+        if (e.key === 'Enter' && gameState !== 'Play') {
+          setPipes([]);
+          setGameState('Play');
+          setScore(0);
+          play();
+        }
       };
-    } else {
-      setObjPos(WALL_WIDTH);
-      setObjHeight(Math.floor(Math.random() * (WALL_HEIGHT - OBJ_GAP)));
-      if (isStart) setScore((score) => score + 1);
-    }
-  }, [isStart, objPos]);
+      document.addEventListener('keydown', handleKeyDown);
+      return () => {
+        document.removeEventListener('keydown', handleKeyDown);
+      };
+    }, [gameState]);
+  
+    useEffect(() => {
+      const handleKeyUp = (e) => {
+        if (e.key === 'ArrowUp' || e.key === ' ') {
+          document.getElementById('narwhal-1').src = 'narwhal.png';
+        }
+      };
+      document.addEventListener('keyup', handleKeyUp);
+      return () => {
+        document.removeEventListener('keyup', handleKeyUp);
+      };
+    }, []);
+  
+    const handleNarwhalClick = () => {
+      if (gameState === 'Play') {
+        setNarwhalDy(-7.7);
+        document.getElementById('narwhal-1').src = 'narwhal-2.png';
+      }
+    };
+  
+    const play = () => {
+      function move() {
+        if (gameState !== 'Play') return;
+        setPipes((prevPipes) => {
+          return prevPipes
+            .map((pipe) => {
+              pipe.left -= moveSpeed;
+              return pipe;
+            })
+            .filter((pipe) => pipe.right > 0);
+        });
+        requestAnimationFrame(move);
+      }
+      requestAnimationFrame(move);
+  
+      function applyGravity() {
+        if (gameState !== 'Play') return;
+        setNarwhalDy((prevDy) => prevDy + gravity);
+        if (
+          narwhalRef.current &&
+          (narwhalRef.current.offsetTop <= 0 ||
+            narwhalRef.current.getBoundingClientRect().bottom >=
+              backgroundRef.current.getBoundingClientRect().bottom)
+        ) {
+          endGame();
+          return;
+        }
+        requestAnimationFrame(applyGravity);
+      }
+      requestAnimationFrame(applyGravity);
+  
+      let pipeSeperation = 160;
+      let pipeGap = 50;
+      function createPipe() {
+        if (gameState !== 'Play') return;
+        if (pipeSeperation > 190) {
+          pipeSeperation = 0;
+          const pipePosi = Math.floor(Math.random() * 43) + 8;
+          const newPipe = {
+            top: pipePosi - 70 + 'vh',
+            left: '100vw',
+            increaseScore: true,
+          };
+          setPipes((prevPipes) => [...prevPipes, newPipe]);
+          const newPipe2 = {
+            top: pipePosi + pipeGap + 'vh',
+            left: '100vw',
+            increaseScore: false,
+          };
+          setPipes((prevPipes) => [...prevPipes, newPipe2]);
+        }
+        pipeSeperation++;
+        requestAnimationFrame(createPipe);
+      }
+      requestAnimationFrame(createPipe);
+      function App() {
+        const [count, setCount] = useState(0);
+        const incrementCount = () => setCount(count + 1);
+        const decrementCount = () => setCount(count - 1);
+        
+        return (
+          <div>
+            <h1>Counter App</h1>
+            <p>Current Count: {count}</p>
+            <button onClick={incrementCount}>Increment</button>
+            <button onClick={decrementCount}>Decrement</button>
+          </div>
+        );
+      }
+    }      
 
-  useEffect(() => {
-    let topObj = narwhalpos >= 0 && narwhalpos < objHeight;
-    let bottomObj =
-      narwhalpos <= WALL_HEIGHT &&
-      narwhalpos >=
-        WALL_HEIGHT - (WALL_HEIGHT - OBJ_GAP - objHeight) - NARWHAL_HEIGHT;
 
-    if (
-      objPos >= OBJ_WIDTH &&
-      objPos <= OBJ_WIDTH + 80 &&
-      (topObj || bottomObj)
-    ) {
-      setIsStart(false);
-      setBirspos(300);
-      setScore(0);
-    }
-  }, [isStart, narwhalpos, objHeight, objPos]);
-  const handler = () => {
-    if (!isStart) setIsStart(true);
-    else if (narwhalpos < narwhal_HEIGHT) setBirspos(0);
-    else setBirspos((narwhalpos) => narwhalpos - 50);
-  };
   return (
-    <Home onClick={handler}>
-      <span>Score: {score}</span>
-      <Background height={WALL_HEIGHT} width={WALL_WIDTH}>
-        {!isStart ? <Startboard>Click To Start</Startboard> : null}
-        <Obj
-          height={objHeight}
-          width={OBJ_WIDTH}
-          left={objPos}
-          top={0}
-          deg={180}
-        />
-        <narwhal
-          height={NARWHAL_HEIGHT}
-          width={NARWHAL_WIDTH}
-          top={narwhalpos}
-          left={100}
-        />
-        <Obj
-          height={WALL_HEIGHT - OBJ_GAP - objHeight}
-          width={OBJ_WIDTH}
-          left={objPos}
-          top={WALL_HEIGHT - (objHeight + (WALL_HEIGHT - OBJ_GAP - objHeight))}
-          deg={0}
-        />
-      </Background>
-    </Home>
+    <div className='game'>
+      <div className='button-container'>
+        <>{buttonArray}</>
+        <div className='controls center'>
+          <div className='game-name'>
+            Simon<sup>&reg;</sup>
+          </div>
+          <div className='score center'>{sequence.length === 0 ? '--' : sequence.length - 1}</div>
+          <Button variant='primary' onClick={() => reset()}>
+            Reset
+          </Button>
+        </div>
+      </div>
+    </div>
   );
+  }
 }
-
-export default App;
-
-const Home = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const Background = styled.div`
-  background-image: url("/Users/jadallred/startup/public/underwater.jpg");
-  background-repeat: no-repeat;
-  background-size: ${(props) => props.width}px ${(props) => props.height}px;
-  width: ${(props) => props.width}px;
-  height: ${(props) => props.height}px;
-  position: relative;
-  overflow: hidden;
-  border: 2px solid black;
-`;
-
-const narwhal = styled.div`
-  position: absolute;
-  background-image: url("/Users/jadallred/startup/public/narwhal.png");
-  background-repeat: no-repeat;
-  background-size: ${(props) => props.width}px ${(props) => props.height}px;
-  width: ${(props) => props.width}px;
-  height: ${(props) => props.height}px;
-  top: ${(props) => props.top}px;
-  left: ${(props) => props.left}px;
-`;
-
-const Obj = styled.div`
-  position: relative;
-  background-image: url("/Users/jadallred/startup/public/pipe-green.png");
-  width: ${(props) => props.width}px;
-  height: ${(props) => props.height}px;
-  left: ${(props) => props.left}px;
-  top: ${(props) => props.top}px;
-  transform: rotate(${(props) => props.deg}deg);
-`;
-
-const Startboard = styled.div`
-  position: relative;
-  top: 49%;
-  background-color: black;
-  padding: 10px;
-  width: 100px;
-  left: 50%;
-  margin-left: -50px;
-  text-align: center;
-  font-size: 20px;
-  border-radius: 10px;
-  color: #fff;
-  font-weight: 600;
-`;
-
-const ScoreShow = styled.div`
-  text-align: center;
-  background: transparent;
-`;

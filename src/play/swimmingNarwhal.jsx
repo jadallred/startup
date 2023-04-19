b76e85b6324b429c1c9d76d8bb7bdc0e2bdbafaa
jadallred <allred.jade@gmail.com>
2023-04-18 20:30:18 -0600

    react

--- src/play/swimmingNarwhal.jsx
+++ src/play/swimmingNarwhal.jsx
@@ -14,145 +14,144 @@ export function SwimmingNarwhal(props) {
   const [sequence, setSequence] = React.useState([]);
   const [playbackPos, setPlaybackPos] = React.useState(0);
 
-  async function onPressed(buttonPosition) {
-    if (allowPlayer) {
-      setAllowPlayer(false);
-      await buttons.get(buttonPosition).ref.current.press();
 
-      if (sequence[playbackPos].position === buttonPosition) {
-        if (playbackPos + 1 === sequence.length) {
-          setPlaybackPos(0);
-          increaseSequence(sequence);
-        } else {
-          setPlaybackPos(playbackPos + 1);
-          setAllowPlayer(true);
+  function Game() {
+      const [move_speed, setMoveSpeed] = useState(2);
+      const [gravity, setGravity] = useState(0.24);
+      const [gameState, setGameState] = useState('Start');
+      const [score, setScore] = useState(0);
+  
+      useEffect(() => {
+          const narwhal = document.querySelector('.narwhal');
+          const img = document.getElementById('narwhal-1');
+          const soundPoint = new Audio('sounds effect/point.mp3');
+          const soundDie = new Audio('sounds effect/die.mp3');
+  
+          // getting narwhal element properties
+          const narwhalProps = narwhal.getBoundingClientRect();
+          // This method returns DOMReact -> top, right, bottom, left, x, y, width and height
+          const background = document.querySelector('.background').getBoundingClientRect();
+          const scoreVal = document.querySelector('.score_val');
+          const message = document.querySelector('.message');
+          const scoreTitle = document.querySelector('.score_title');
+          img.style.display = 'none';
+          message.classList.add('messageStyle');
+          document.addEventListener('keydown', (e) => {
+              if(e.key == 'Enter' && gameState != 'Play'){
+                  document.querySelectorAll('.pipe_sprite').forEach((e) => {
+                      e.remove();
+                  });
+                  img.style.display = 'block';
+                  narwhal.style.top = '40vh';
+                  setGameState('Play');
+                  message.innerHTML = '';
+                  scoreTitle.innerHTML = 'Score : ';
+                  scoreVal.innerHTML = '0';
+                  message.classList.remove('messageStyle');
+                  play();
+              }
+          });
+  
+          function play(){
+            function move(){
+                if(gameState != 'Play') return;
+                let pipeSprite = document.querySelectorAll('.pipe_sprite');
+                pipeSprite.forEach((element) => {
+                    let pipeSpriteProps = element.getBoundingClientRect();
+                    narwhalProps = narwhal.getBoundingClientRect();
+                    if(pipeSpriteProps.right <= 0){
+                        element.remove();
+                    }else{
+                        if(narwhalProps.left < pipeSpriteProps.left + pipeSpriteProps.width && narwhalProps.left + narwhalProps.width > pipeSpriteProps.left && narwhalProps.top < pipeSpriteProps.top + pipeSpriteProps.height && narwhalProps.top + narwhalProps.height > pipeSpriteProps.top){
+                            setGameState('End');
+                            message.innerHTML = 'Game Over'.fontcolor('red') + '<br>Press Enter To Restart';
+                            message.classList.add('messageStyle');
+                            img.style.display = 'none';
+                            soundDie.play();
+                            return;
+                        }else{
+                            if(pipeSpriteProps.right < narwhalProps.left && pipeSpriteProps.right + move_speed >= narwhalProps.left && element.increase_score == '1'){
+                                setScore(score + 1);
+                                soundPoint.play();
+                            }
+                            element.style.left = pipeSpriteProps.left - move_speed + 'px';
+                        }
+                    }
+                });
+                requestAnimationFrame(move);
+            }
+            requestAnimationFrame(move);
+
+            let narwhalDY = 0;
+            function applyGravity(){
+                if(gameState != 'Play') return;
+                narwhalDY = narwhalDY + gravity;
+                document.addEventListener('keydown', (e) => {
+                    if(e.key == 'ArrowUp' || e.key == ' '){
+                        img.src = 'narwhal-2.png';
+                        narwhalDY = -7.7
+                    }
+                });
+                let narwhalTop = narwhalProps.top + narwhalDY;
+                if(narwhalTop <= background.top || narwhalTop + narwhalProps.height >= background.bottom){
+                    setGameState('End');
+                    message.innerHTML = 'Game Over'.fontcolor('red') + '<br>Press Enter To Restart';
+                    message.classList.add('messageStyle');
+                    img.style.display = 'none';
+                    soundDie.play();
+                    return;
+                }else{
+                    narwhal.style.top = narwhalTop + 'px';
+                }
+                requestAnimationFrame(applyGravity);
+            }
+            requestAnimationFrame(applyGravity);
+
+            let count = 0;
+            function createPipe(){
+                if(gameState != 'Play') return;
+                count++;
+                if(count % 100 == 0){
+                    let pipeSpriteHeight = 100 + Math.floor(Math.random() * 200);
+                    let pipeBottom = document.createElement('div');
+                    let pipeTop = document.createElement('div');
+                    pipeBottom.setAttribute('class', 'pipe_sprite');
+                    pipeTop.setAttribute('class', 'pipe_sprite');
+                    pipeBottom.style.height = pipeSpriteHeight + 'px';
+                    pipeBottom.style.left = '110vw';
+                    pipeTop.style.bottom = pipeSpriteHeight + 150 + 'px';
+                    pipeTop.style.left = '110vw';
+                    pipeTop.style.transform = 'rotate(180deg)';
+                    pipeTop.increase_score = '1';
+                    document.querySelector('.background').appendChild(pipeBottom);
+                    document.querySelector('.background').appendChild(pipeTop);
+                }
+                requestAnimationFrame(createPipe);
+            }
+            requestAnimationFrame(createPipe);
         }
-      } else {
-        saveScore(sequence.length - 1);
-        mistakeSound.play();
-        await buttonDance();
-      }
-    }
-  }
 
-  async function reset() {
-    setAllowPlayer(false);
-    setPlaybackPos(0);
-    await buttonDance(1);
-    increaseSequence([]);
-
-    // Let other players know a new game has started
-    GameNotifier.broadcastEvent(userName, GameEvent.Start, {});
-  }
-
-  function increaseSequence(previousSequence) {
-    const newSequence = [...previousSequence, getRandomButton()];
-    setSequence(newSequence);
-  }
-
-  // Demonstrates updating state objects based on changes to other state.
-  // All setState calls are asynchronous and so you need to wait until
-  // that state is updated before you can update dependent functionality.
-  React.useEffect(() => {
-    if (sequence.length > 0) {
-      const playSequence = async () => {
-        await delay(500);
-        for (const btn of sequence) {
-          await btn.ref.current.press();
+        // cleanup
+        return () => {
+            document.removeEventListener('keydown');
         }
-        setAllowPlayer(true);
-      };
-      playSequence();
-    }
-  }, [sequence]);
+    }, [gameState]);
 
-  async function buttonDance(laps = 5) {
-    for (let step = 0; step < laps; step++) {
-      for (const btn of buttons.values()) {
-        await btn.ref.current.press(100, false);
-      }
-    }
-  }
-
-  function getRandomButton() {
-    let b = Array.from(buttons.values());
-    return b[Math.floor(Math.random() * b.length)];
-  }
-
-  async function saveScore(score) {
-    const date = new Date().toLocaleDateString();
-    const newScore = { name: userName, score: score, date: date };
-
-    try {
-      const response = await fetch('/api/score', {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify(newScore),
-      });
-
-      // Let other players know the game has concluded
-      GameNotifier.broadcastEvent(userName, GameEvent.End, newScore);
-
-      // Store what the service gave us as the high scores
-      const scores = await response.json();
-      localStorage.setItem('scores', JSON.stringify(scores));
-    } catch {
-      // If there was an error then just track scores locally
-      updateScoresLocal(newScore);
-    }
-  }
-
-  function updateScoresLocal(newScore) {
-    let scores = [];
-    const scoresText = localStorage.getItem('scores');
-    if (scoresText) {
-      scores = JSON.parse(scoresText);
-    }
-
-    let found = false;
-    for (const [i, prevScore] of scores.entries()) {
-      if (newScore > prevScore.score) {
-        scores.splice(i, 0, newScore);
-        found = true;
-        break;
-      }
-    }
-
-    if (!found) {
-      scores.push(newScore);
-    }
-
-    if (scores.length > 10) {
-      scores.length = 10;
-    }
-
-    localStorage.setItem('scores', JSON.stringify(scores));
-  }
-
-  // We use React refs so the game can drive button press events
-  buttons.set('button-top-left', { position: 'button-top-left', ref: React.useRef() });
-  buttons.set('button-top-right', { position: 'button-top-right', ref: React.useRef() });
-  buttons.set('button-bottom-left', { position: 'button-bottom-left', ref: React.useRef() });
-  buttons.set('button-bottom-right', { position: 'button-bottom-right', ref: React.useRef() });
-
-  const buttonArray = Array.from(buttons, ([key, value]) => {
-    return <SimonButton key={key} ref={value.ref} position={key} onPressed={() => onPressed(key)}></SimonButton>;
-  });
-
-  return (
-    <div className='game'>
-      <div className='button-container'>
-        <>{buttonArray}</>
-        <div className='controls center'>
-          <div className='game-name'>
-            Simon<sup>&reg;</sup>
+    return (
+      <div className='game'>
+        <div className='button-container'>
+          <>{buttonArray}</>
+          <div className='controls center'>
+            <div className='game-name'>
+              Simon<sup>&reg;</sup>
+            </div>
+            <div className='score center'>{sequence.length === 0 ? '--' : sequence.length - 1}</div>
+            <Button variant='primary' onClick={() => reset()}>
+              Reset
+            </Button>
           </div>
-          <div className='score center'>{sequence.length === 0 ? '--' : sequence.length - 1}</div>
-          <Button variant='primary' onClick={() => reset()}>
-            Reset
-          </Button>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
